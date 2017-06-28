dfcb4bb2ef96d4b1ef6174cfb9af48b3ab50ba12
Adam J. Lincoln <adamjlincoln@gmail.com>
2017-06-28 19:05:49 -0400

    Dependencies

--- setup.py
+++ setup.py
@@ -12,6 +12,10 @@ install_requires = [
     # List your project dependencies here.
     # For more details, see:
     # http://packages.python.org/distribute/setuptools.html#declaring-dependencies
+    'simplejson',
+    'sqlalchemy',
+    'zope.sqlalchemy',
+    'requests',
 ]
 
 
